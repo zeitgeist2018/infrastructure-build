6700c05030d7eca3ce090b1c787d9d895fe63d65
cristian <cristianlobomartin@gmail.com>
2020-04-10 22:11:51 +0200

    Remove old comments

--- install-docker.sh
+++ install-docker.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# sudo apt-get update -y > /dev/null 2>&1
-# sudo apt-get install systemd -y > /dev/null 2>&1
-# sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-# sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-trusty main'
 sudo apt-get update -y > /dev/null 2>&1
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common > /dev/null 2>&1
 sudo apt-get install -y docker.io > /dev/null 2>&1
