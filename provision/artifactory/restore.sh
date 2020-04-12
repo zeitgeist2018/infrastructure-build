a32aaca17f3c6adf45fa6fb859cc874dd0eb0722
cristian <cristianlobomartin@gmail.com>
2020-04-12 17:58:09 +0200

    Artifactory is configured automatically now

---
+++ provision/artifactory/restore.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+RED='\n\033[0;31m'
+GREEN='\n\033[0;32m'
+NC='\033[0m'
+
+HOST="http://localhost:8081"
+COOKIES_FILE="artifactory-cookies"
+BACKUP_FILE="/home/vagrant/provision/backup.zip"
+CONTAINER_BACKUP_FILE="/home/backup.zip"
+DEFAULT_USER="admin"
+DEFAULT_PWD="password"
+
+function waitForReadiness() {
+  PING_URL="$HOST/artifactory/api/system/ping"
+  echo -ne "Waiting for artifactory to be ready"
+  while :; do
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" $PING_URL)
+    if [ $RESPONSE_CODE -eq 200 ] || [ $RESPONSE_CODE -eq 401 ]; then
+      break
+    fi
+    echo -ne "."
+    sleep 2
+  done
+  printf "\n${GREEN}Artifactory ready${NC}"
+}
+
+function login() {
+  printf "\n\nLOGIN INTO ARTIFACTORY"
+  LOGIN_BODY="{\"user\": \"$DEFAULT_USER\",\"password\": \"$DEFAULT_PWD\",\"type\":\"login\"}"
+  LOGIN_URL="$HOST/artifactory/ui/auth/login"
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" -X POST $LOGIN_URL -H "Content-Type: application/json" --data "$LOGIN_BODY" --cookie-jar $COOKIES_FILE)
+  if [ $RESPONSE_CODE -eq 200 ]; then
+    printf "${GREEN}Artifactory login successful${NC}"
+  else
+    printf "${RED}There has been an error while loging into Artifactory${NC}"
+    exit 1
+  fi
+}
+
+function restoreBackup() {
+  printf "\n\nRESTORING BACKUP"
+  RESTORE_URL="$HOST/artifactory/ui/artifactimport/system"
+  RESTORE_BODY="{\"path\": \"$CONTAINER_BACKUP_FILE\",\"excludeContent\":false,\"excludeMetadata\":false,\"verbose\":false,\"zip\":true,\"action\":\"system\"}"
+  CONTAINER_ID=$(docker ps -aqf "name=^artifactory$")
+  docker cp $BACKUP_FILE "$CONTAINER_ID":$CONTAINER_BACKUP_FILE
+  curl -X POST $RESTORE_URL -H "Content-Type: application/json" -H "X-Requested-With: artUI" -H "Request-Agent: artifactoryUI" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15" --data "$RESTORE_BODY" --cookie $COOKIES_FILE
+  RESPONSE_CODE=$(curl -X POST $RESTORE_URL -s -o /dev/null -w "%{http_code}" -H "Content-Type: application/json" -H "X-Requested-With: artUI" -H "Request-Agent: artifactoryUI" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15" --data "$RESTORE_BODY" --cookie $COOKIES_FILE)
+  if [ $RESPONSE_CODE -eq 200 ]; then
+    printf "${GREEN}Artifactory has been configured successfully!${NC}"
+  else
+    printf "${RED}There has been an error while restoring artifactory backup. Code: $RESPONSE_CODE ${NC}"
+  fi
+  rm $COOKIES_FILE
+}
+
+waitForReadiness
+login
+restoreBackup
