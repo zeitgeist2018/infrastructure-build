5cd26ae859d3c8aefe4b5e2197382ffaf9d87399
cristian <cristianlobomartin@gmail.com>
2020-04-16 00:31:51 +0200

    Update Jenkins config

--- backup-jenkins.sh
+++ backup-jenkins.sh
@@ -6,7 +6,7 @@ MASTER_FOLDER="./provision/jenkins/master"
 
 function backup() {
   echo "********** BACKING UP JENKINS **********"
-  docker cp $MASTER_ID:/usr/share/jenkins/ref/jobs $MASTER_FOLDER/jobs
+  docker cp $MASTER_ID:/var/jenkins_home/jobs $MASTER_FOLDER
   docker cp $MASTER_ID:/var/jenkins_home/config.xml $MASTER_FOLDER/config/config.xml
   docker cp $MASTER_ID:/var/jenkins_home/com.dabsquared.gitlabjenkins.connection.GitLabConnectionConfig.xml $MASTER_FOLDER/config/com.dabsquared.gitlabjenkins.connection.GitLabConnectionConfig.xml
   docker cp $MASTER_ID:/var/jenkins_home/plugins.txt $MASTER_FOLDER/plugins.txt
