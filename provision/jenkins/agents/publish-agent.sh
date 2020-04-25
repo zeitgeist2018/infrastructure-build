e29ecaa0de81784aae62a1c3719ca87e898ff94d
cristian <cristianlobomartin@gmail.com>
2020-04-25 20:33:57 +0200

    Add Jenkins java agent

---
+++ provision/jenkins/agents/publish-agent.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo "You must provide the agent name to publish"
+    exit 1
+fi
+
+DOMAIN="artifactory-jcr.dev.local"
+REPO="$DOMAIN/docker-local"
+PREFIX="jenkins-agent"
+AGENT=$1
+VERSION=${2:-latest}
+TAG="$REPO/$PREFIX-$AGENT:$VERSION"
+
+docker build -t "$TAG" "$AGENT"
+#docker login -u cristianlm -p cristianlm $DOMAIN
+docker push "$TAG"
+docker rmi "$TAG"
